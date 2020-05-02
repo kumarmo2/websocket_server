e03985de52d3ea691115adb2c9a1faa7bd301bff
kumarmo2 <mohitkumar1015795@gmail.com>
2020-05-02 14:01:17 +0530

    set websockets basic setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use async_tungstenite::WebSocketStream;
+use futures::future;
+use futures_util::sink::SinkExt;
+use smol::{self, block_on, Async, Task};
+use std::net::TcpListener;
+use std::thread;
+use tungstenite::Message;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello World!");
+    let cpus = num_cpus::get().max(1);
+    for _ in 0..cpus - 1 {
+        thread::spawn(move || smol::run(future::pending::<()>()));
+    }
+    block_on(async move {
+        let listener =
+            Async::<TcpListener>::bind("127.0.0.1:9001").expect("could not bind to the localhost");
+        loop {
+            let (stream, _) = listener.accept().await.unwrap();
+            let mut ws_stream: WebSocketStream<_> = async_tungstenite::accept_async(stream)
+                .await
+                .expect("could not accept new connection");
+            Task::spawn(async move {
+                loop {
+                    println!(
+                        "sending message, from threadId: {:?}",
+                        thread::current().id()
+                    );
+                    ws_stream.send(Message::text("sfsdfsdff")).await.unwrap();
+                    smol::Timer::after(std::time::Duration::from_secs(2)).await;
+                }
+            })
+            .detach();
+        }
+    });
 }
